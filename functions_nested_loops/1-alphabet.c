501131ab46e9768f46d292e45810dcaf87bd545f
Jorix2007 <jabirbusines@gmail.com>
2025-12-12 11:41:40 +0400

    local

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet  Prins the alphabet in lowercase followed by a new line
+*
+*Return: void
+*/
+void print_alphabet(void)
+{
+    char letter;
+
+    letter = 'a';
+    while (letter <= 'z');
+    {
+        _putchar(letter);
+        letter ++;
+    }
+    _putchar('\n');
+}
+

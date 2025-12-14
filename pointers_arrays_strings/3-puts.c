fa0520129b8e98b23b7d6baacbb05d666ddd427e
Jorix2007 <jabirbusines@gmail.com>
2025-12-14 10:57:48 +0400

    local

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-puts.c
+ * Auth: Jabir Hasanli
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: The string to print
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+

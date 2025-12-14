f1e06c26e1b8397d7e30b55b448b726ccb5e5d5c
Jorix2007 <jabirbusines@gmail.com>
2025-12-14 10:52:57 +0400

    local

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+/*
+ * File: 1-swap.c
+ * Auth: Jabir Hasanli
+ */
+
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ * @a: The pointer to an int.
+ * @b: The pointer to an int.
+ */
+
+void swap_int(int *a, int *b)
+{
+int p = *a;
+int c = *b;
+*a = c;
+*b = p;
+}
+

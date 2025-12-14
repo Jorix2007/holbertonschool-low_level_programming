e43878383eb71a5fe898ca3737dfd61a797ad422
Jorix2007 <jabirbusines@gmail.com>
2025-12-14 11:02:38 +0400

    lol

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+/*
+ * File: 6-puts2.c
+ * Auth: Jabir Hasanli
+ */
+
+#include "main.h"
+
+/**
+ * puts2 - Function that prints every other character of a string.
+ * @str: The string to modify
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

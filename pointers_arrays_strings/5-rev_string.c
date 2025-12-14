7e49a6c985d475623a1e0cf26486b5f6b5660dbf
Jorix2007 <jabirbusines@gmail.com>
2025-12-14 11:01:32 +0400

    lol

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+/*
+ * File: 5-rev_string.c
+ * Auth: Jabir Hasanli
+ */
+
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to modify
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int b = 0;
+	char tmp;
+
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+
+	for (b = b - 1; b > i; b--)
+	{
+		tmp = s[i];
+		s[i] = s[b];
+		s[b] = tmp;
+		i++;
+	}
+}

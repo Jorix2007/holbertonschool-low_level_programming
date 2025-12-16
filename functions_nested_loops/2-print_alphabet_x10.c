a215331a7c80887670be47ad77945c0a1d7718da
Jorix2007 <jabirbusines@gmail.com>
2025-12-16 10:22:21 +0400

    lol

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,14 @@ void print_alphabet_x10(void)
     char letter;
     int i;
     letter = 'a';
-    for (i = 0; i < 10; i++)
+    for (i = 0; i <= 10; i++)
     {
         while (letter <= 'z')
         {
             _putchar(letter);
             letter++;
         }
+        _putchar('\n');
     }
-    _putchar('\n');
+    
 }

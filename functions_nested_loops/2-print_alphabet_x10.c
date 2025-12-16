7a49e2b8ca90eb874c50a7afc783a23b3073438e
Jorix2007 <jabirbusines@gmail.com>
2025-12-16 10:09:41 +0400

    local

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,15 @@
-x
+#include "main.h"
+
+int void print_alphabet_x10(void)
+{
+    char letter
+
+    letter = 'a';
+    for (i = 0, 10, i++)
+    {
+        while (letter <= 'z')
+            _putchar(letter);
+            letter++;
+    }
+    _putchar('\n')
+}

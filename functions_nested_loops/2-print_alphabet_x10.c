306bad748119c1a70397f825626e3c99a562d411
Jorix2007 <jabirbusines@gmail.com>
2025-12-16 10:25:29 +0400

    local

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,13 @@ void print_alphabet_x10(void)
 {
     char letter;
     int i;
-    letter = 'a';
-    for (i = 0; i <= 10; i++)
+
+    /* Loop from 0 to 9 (10 times) */
+    for (i = 0; i < 10; i++)
     {
+        /* RESET letter to 'a' at the start of every row */
+        letter = 'a'; 
+        
         while (letter <= 'z')
         {
             _putchar(letter);

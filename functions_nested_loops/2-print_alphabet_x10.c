4889f2781305ed76ab0c56af1956732633c48d78
Jorix2007 <jabirbusines@gmail.com>
2025-12-16 10:16:52 +0400

    local

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,9 @@
 void print_alphabet_x10(void)
 {
     char letter;
-
+    int i;
     letter = 'a';
-    for (i = 0, 10, i++)
+    for (i = 0, i < 10, i++)
     {
         while (letter <= 'z')
             _putchar(letter);

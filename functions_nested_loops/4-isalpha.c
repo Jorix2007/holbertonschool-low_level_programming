eca906ac60fce6cf43c74cf7e542e1caa131c535
Jorix2007 <jabirbusines@gmail.com>
2025-12-17 09:48:42 +0400

    lol

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+
+
+*/
+int letter;
+int _isalpha(int c);
+{
+    if (c > 'a' && c < 'z')
+        _putchar(1 + '0');
+    else if (c > 'A' && c < 'Z')
+        _putchar(1 + '0')
+    else
+        _putchar('0')
+}

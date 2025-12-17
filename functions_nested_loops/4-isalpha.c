2532d87ae04b32d73608dc3509f2a0ff9330e3d5
Jorix2007 <jabirbusines@gmail.com>
2025-12-17 09:56:10 +0400

    lol

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
-/*
-
-
-*/
+/**
+ * _isalpha - checks for alphabetic character
+ * Return: 1 if c is a letter, 0 otherwise
+ */
 int _isalpha(int c)
 {
-    if (c > 'a' && c < 'z')
-        _putchar(1 + '0');
-    else if (c > 'A' && c < 'Z')
-        _putchar(1 + '0');
-    else
-        return(0);
+    /* Проверяем диапазон маленьких букв ИЛИ больших букв */
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return (1);
+    }
+    
+    return (0);
 }

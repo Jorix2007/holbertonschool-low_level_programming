f82103a42436a0a4f475e1acbcae155cc18d103a
Jorix2007 <jabirbusines@gmail.com>
2025-12-24 10:27:08 +0400

    local

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,6 +24,7 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif
 

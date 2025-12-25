df8d779d3cf53c5a9ec10fec16a734dd271692c1
Jorix2007 <jabirbusines@gmail.com>
2025-12-25 12:13:53 +0400

    local

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,33 +11,21 @@ char *cap_string(char *s)
     int i;
     int capitalize_next = 1;
     char separators[] = " \t\n,;.!?\"(){}";
-
     for (i = 0; s[i] != '\0'; i++)
     {
-        int is_separator = 0;
+        if (capitalize_next && s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] = s[i] - ('a' - 'A');
+        }
+        capitalize_next = 0;
         for (int j = 0; separators[j] != '\0'; j++)
         {
             if (s[i] == separators[j])
             {
-                is_separator = 1;
+                capitalize_next = 1;
                 break;
             }
         }
-
-        if (is_separator)
-        {
-            capitalize_next = 1; /* Next character should be capitalized */
-        }
-        else if (capitalize_next && s[i] >= 'a' && s[i] <= 'z')
-        {
-            s[i] = s[i] - ('a' - 'A'); /* Capitalize the character */
-            capitalize_next = 0; /* Reset the flag */
-        }
-        else
-        {
-            capitalize_next = 0; /* Reset the flag */
-        }
     }
-
     return s;
 }

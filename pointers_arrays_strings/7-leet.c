4607f500f01fd2a58f04c23884c31544bf53ecfb
Jorix2007 <jabirbusines@gmail.com>
2025-12-28 11:43:50 +0400

    local

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,27 +1,32 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * leet - encodes a string into 1337
  * @s: the string to encode
  *
- * Return: the encoded string
+ * Return: pointer to the encoded string
  */
 char *leet(char *s)
 {
     int i, j;
+    char subs[] = "aAeEoOtTlL";
+    char repl[] = "4433007711";
 
-    char letters[] = "aAeEoOtTlL";
-    char numbers[] = "4433007711";
+    if (s == NULL)
+        return (s);
 
-    for(i = 0; s[i] != '\0'; i++)
+    for (i = 0; s[i] != '\0'; i++)
     {
-        for(j = 0; j < 10; i++)
+        for (j = 0; j < 10; j++)
         {
-            if(s[i] == letters[j])
+            if (s[i] == subs[j])
             {
-                s[i] = numbers[j];
+                s[i] = repl[j];
+                break; 
             }
         }
     }
-    return(s);
+
+    return (s);
 }

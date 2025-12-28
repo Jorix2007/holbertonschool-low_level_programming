a0e1484878442779e80bfa2c190a7031628f8915
Jorix2007 <jabirbusines@gmail.com>
2025-12-28 13:32:00 +0400

    local

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,17 +1,24 @@
 #include "main.h"
+
 /**
- * print_numbers - Prints the numbers from 0 to 14 ten times
- *
- * Return: void
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  */
 void more_numbers(void)
 {
     int i, num;
+
     for (i = 0; i < 10; i++)
     {
         for (num = 0; num <= 14; num++)
         {
-            _putchar(num + '0');
+            /* If number is greater than 9, print the first digit (1) */
+            if (num > 9)
+            {
+                _putchar((num / 10) + '0');
+            }
+            
+            /* Always print the last digit */
+            _putchar((num % 10) + '0');
         }
         _putchar('\n');
     }

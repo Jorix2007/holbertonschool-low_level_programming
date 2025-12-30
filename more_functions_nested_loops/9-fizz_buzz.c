a786ed406674dfdedd14cffafd4514236d8e22b6
Jorix2007 <jabirbusines@gmail.com>
2025-12-30 10:02:45 +0400

    local

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,24 +6,21 @@ void FizzBuzz(void)
     int i, j;
     for(i = 0; i <= 100; i++)
     {
-        for(; j <= i; j++)
+        if(((i % 3) == 0) && ((i % 5) == 0))
         {
-            if((i % j) == 3)
-            {
-                printf('Fizz');
-            }
-            else if((i % j) == 5)
-            {
-                printf('Buzz');
-            }
-            else if(((i % j) == 3) && ((i % j) == 5))
-            {
-                printf('FizzBuzz');
-            }
-            else
-            {
-                printf(i);
-            }
+            printf("Fizz");
+        }
+        else if((i % 5) == 0)
+        {
+            printf("Buzz");
+        }
+        else if((i % 3) == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else
+        {
+            printf("%d", i);
         }
     }
 }

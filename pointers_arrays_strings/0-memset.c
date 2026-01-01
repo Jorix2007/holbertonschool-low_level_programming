c4aed64fd6268a1bfc058bc8e3a96b196d0ddb56
Jorix2007 <jabirbusines@gmail.com>
2026-01-02 02:00:44 +0400

    local

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i; /* Используем unsigned, так как n тоже unsigned */
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-    return (s); /* Не забудь точку с запятой */
+	return (s);
 }

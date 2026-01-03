9ac193ec5bbf5421b1dbbc07ca1ac40936ca239d
Jorix2007 <jabirbusines@gmail.com>
2026-01-04 01:50:17 +0400

    local

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -6,35 +6,21 @@
  * @needle: The substring to look for
  *
  * Return: Pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	/* Edge case: if needle is empty, return haystack (standard rule) */
-	if (*needle == '\0')
-		return (haystack);
-
-	/* Loop through the haystack */
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		/* * For each character in haystack, check if the 
-		 * needle matches starting from here 
-		 */
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			/* If characters don't match, break the inner loop */
 			if (haystack[i + j] != needle[j])
 				break;
 		}
-
-		/* * If we reached the end of needle (needle[j] is \0),
-		 * it means we matched everything!
-		 */
 		if (needle[j] == '\0')
 			return (&haystack[i]);
 	}
-
 	return (NULL);
 }

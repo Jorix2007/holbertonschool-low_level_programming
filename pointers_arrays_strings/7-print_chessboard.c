db997cedd63b74f3024284dd5fa4bf6577fd99ac
Jorix2007 <jabirbusines@gmail.com>
2026-01-04 01:58:45 +0400

    local

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 
 /**
  * print_chessboard - Prints the chessboard
- * @a: The chessboard to be printed (pointer to array of 8 chars)
+ * @a: The chessboard to be printed
  *
  * Return: void
  */
@@ -10,15 +10,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	/* Проходим по 8 строкам */
 	for (i = 0; i < 8; i++)
 	{
-		/* Проходим по 8 колонкам в каждой строке */
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
-		/* Переход на новую строку после печати ряда */
 		_putchar('\n');
 	}
 }

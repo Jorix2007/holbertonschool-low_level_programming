4e477ad22572d732092e70623356a157621c5b86
Jorix2007 <jabirbusines@gmail.com>
2026-01-07 23:09:04 +0400

    local

---
+++ recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Jabir Hasanli
+ * Desc: Header file containing prototypes for all functions
+ *       used in the recursion directory.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif

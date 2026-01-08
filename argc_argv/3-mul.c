d5780ad40a60bc3548a0fc926730c1d0e54ed089
Jorix2007 <jabirbusines@gmail.com>
2026-01-08 23:18:02 +0400

    local

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
-    int multi;
-    int i;
-    if(argc > 1)
-    {
-        for (i = 0; i < argc; i++)
-        {
-            multi *= atoi(argv[i]);
-        }
-        printf("%d\n", multi);
-    }
-    else
-    {
-        printf("Error");
-        return(1);
-    }
-    return(0)
+	int result;
+
+	/* We need exactly 3 arguments: ./program, num1, num2 */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* atoi converts the string arguments to integers */
+	result = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", result);
+
+	return (0);
 }

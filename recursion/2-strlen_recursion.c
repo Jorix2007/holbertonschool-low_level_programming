0dbb9a6f7d93cc22af093dde98587c49c174fd4b
Jorix2007 <jabirbusines@gmail.com>
2026-01-08 09:38:21 +0400

    locla

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,18 +2,16 @@
 
 /**
  * _strlen_recursion - Returns the length of a string
- * @s: The string to measure
+ * @s: The string to be measured
  *
  * Return: The length of the string
  */
 int _strlen_recursion(char *s)
 {
-	/* Базовый случай: если конец строки, длина равна 0 */
 	if (*s == '\0')
 	{
 		return (0);
 	}
 
-	/* Рекурсивный шаг: 1 (текущий символ) + длина остатка */
 	return (1 + _strlen_recursion(s + 1));
 }

889b2dc09608c2fea74332f119b3076c241ca4d1
Jorix2007 <jabirbusines@gmail.com>
2026-01-10 18:56:35 +0400

    lol

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,1 +1,15 @@
-x
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Jabir Hasanli
+ * Desc: Header file containing prototypes for all functions
+ *       used in the malloc_free directory.
+ */
+
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+
+#endif

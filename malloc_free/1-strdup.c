54e53f4c456f426bf44efc5da53892b00a367d00
Jorix2007 <jabirbusines@gmail.com>
2026-01-14 01:40:03 +0400

    local

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,41 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL if it fails
+ */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    int i, len = 0;
+	char *duplicate;
+	unsigned int i, len;
 
-    /* 1. Handle NULL input */
-    if (str == NULL)
-    {
-        return (NULL);
-    }
+	if (str == NULL)
+		return (NULL);
 
-    /* 2. Calculate the length of the string */
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-    /* 3. Allocate memory: Length + 1 for the null terminator */
-    duplicate = malloc((len + 1) * sizeof(char));
+	duplicate = malloc(sizeof(char) * (len + 1));
 
-    /* 4. Check if malloc failed */
-    if (duplicate == NULL)
-    {
-        return (NULL);
-    }
+	if (duplicate == NULL)
+		return (NULL);
 
-    /* 5. Copy the characters */
-    for (i = 0; i < len; i++)
-    {
-        duplicate[i] = str[i];
-    }
+	for (i = 0; i < len; i++)
+	{
+		duplicate[i] = str[i];
+	}
 
-    /* 6. Add the null terminator at the end */
-    duplicate[len] = '\0';
+	duplicate[len] = '\0';
 
-    /* 7. Return the new string */
-    return (duplicate);
+	return (duplicate);
 }

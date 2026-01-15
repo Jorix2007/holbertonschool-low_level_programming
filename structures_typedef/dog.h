5a89d1255b7ceb081bc0557f48ce8e18d97c6c66
Jorix2007 <jabirbusines@gmail.com>
2026-01-15 10:16:43 +0400

    local

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A dog's basic info
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: Longer description of the struct
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif

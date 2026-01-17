62d3ee53706da7bf7276b7f4c92347c889e2a2bf
Jorix2007 <jabirbusines@gmail.com>
2026-01-17 23:25:08 +0400

    local

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,24 +3,22 @@
 
 /**
  * print_dog - prints a struct dog
- * @d: pointer to struct dog
+ * @d: pointer to struct dog to print
+ *
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
-	/* 1. Если самой структуры нет - ничего не делаем */
 	if (d == NULL)
 		return;
 
-	/* 2. Проверяем Имя */
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
-	/* 3. Возраст печатаем всегда (число не может быть NULL) */
 	printf("Age: %f\n", d->age);
 
-	/* 4. Проверяем Владельца */
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
